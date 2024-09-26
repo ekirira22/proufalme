195cf64bcd51978a3c9072e51623a0b1be33874b
Eric Maranga <ekirira22@gmail.com>
2024-09-26 09:39:56 +0300

    added supabase database

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,13 @@ export default function RootLayout({
             <Sidebar />
             <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#333333] my-2 rounded-lg">
               <Header />
-              <Searchbar />
+              {/* <Searchbar /> */}
               <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
                 <div className="flex-1 h-fit pb-40">
                   { children }
                 </div>
                 <div className="xl:sticky relative top-0 h-fit">
-                  <TopPlay />
+                  {/* <TopPlay /> */}
                 </div>
               </div>
             </div>
